813097a1e9c6bd9b0ca8558d570bfb65ad3a4adf
Kiana Kavoosi <kikavoosi@aol.com>
2026-03-22 10:27:24 -0700

    event details

--- src/sizdah-bedar/Sizdah.tsx
+++ src/sizdah-bedar/Sizdah.tsx
@@ -1,18 +1,6 @@
-import {
-    Field,
-    FieldGroup,
-    FieldLabel
-  } from "@/components/ui/field"
-  import {
-    MapPinIcon,
-  } from "lucide-react"
 import { Card } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
-import { Input } from "@/components/ui/input";
-import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
-import { Badge } from "@/components/ui/badge";
 import InfoCard from "./InfoCard";
 
 function Sizdah() {
@@ -21,7 +9,7 @@ function Sizdah() {
         <h1 className="text-4xl font-sans text-center text-foreground">Sizdah bedar picnic</h1>
         <p className="text-3xl my-4">🌸 🌷 🌼</p>
         <p className="text-ring text-center">
-            Let's celebrate sizdah bedar at Kenneth Hahn
+            Hello friends! I'm hosting a picnic in celebration of <a className="text-muted-foreground underline" href="https://en.wikipedia.org/wiki/Sizdah_Be-dar" target="_blank" rel="noopener noreferrer">sizdah bedar</a>. The plan is to spend the day at Kenneth Hahn yapping, snacking, and engaging in tomfoolery. Come through for as long or short as you wish. RSVP <a className="text-muted-foreground underline" href="https://forms.gle/LVzDtEqBJS968uCz9">here</a> by April 1st!
         </p>
         
         <Card className="flex flex-row px-4 my-4 bg-foreground text-background">
@@ -38,96 +26,59 @@ function Sizdah() {
             </div>
         </Card>
         <Separator className='max-w-2xl' />
-        <div className="grid grid-cols-2 gap-4 my-4 max-w-2xl">
-            <InfoCard icon="🧺" title="What to bring">
-                <p>A blanket or lawn chair · Sunscreen · A dish to share · Good vibes · Your favorite outdoor game · Layers for the afternoon breeze</p>
+        <div className='max-w-2xl my-4'>
+            <InfoCard icon="🍓" title="Food & drinks">
+                <p>I will be providing some food that embodies springtime for me (+ paper plates and cutlery) </p>
+                <ul className="text-center">
+                    <li><b>sabzi khordan platter</b><br/> persian snacking herbs, cucumber, radishes, walnuts, cheese</li>
+                    <li><b>kuku sabzi</b><br/> herb frittatta</li>
+                    <li><b>kotlet</b><br/> fried patties of meat, potato, and onion </li>
+                    <li><b>chai & sheerini</b><br/> black tea and small sweets</li>
+                </ul>
+                <p>I feel blessed to know most of your tummys pretty well, but lmk if you have any dietary restrictions. Contributions are welcome but not required. Lmk if you want help deciding what to bring!</p>
             </InfoCard>
+        </div>
+        <div className="grid grid-cols-2 gap-4 max-w-2xl">
             <InfoCard icon="👀" title="Where to find us">
-                <p>will update with picnic table location on the morning of</p>
+                <p>We'll be posted up at Kenneth Hahn Park. Hoping to snag a table S/SE of the lake. I will send out an update on the morning of.</p>
+                <p>A couple map options for reference:</p>
+                <ul>
+                    <li><a className="text-muted-foreground underline" href="https://file.lacounty.gov/SDSInter/dpr/213796_KHTrails1.pdf" target="_blank" rel="noopener noreferrer">LA county trail map</a></li>
+                    <li><a className="text-muted-foreground underline" href="https://csparks.maps.arcgis.com/apps/instant/basic/index.html?appid=065b067caa204e8da48d4b53c9483ab0&UNITNBR=516" target="_blank" rel="noopener noreferrer">California state parks map</a></li>
+                    <li><a className="text-muted-foreground underline" href="https://ontheworldmap.com/usa/city/los-angeles/kenneth-hahn-state-recreation-area-map.jpg" target="_blank" rel="noopener noreferrer">Mystery internet map</a></li>
+                    
+                </ul>
             </InfoCard>
-            <InfoCard icon="🚌" title='Getting there'>
-                <p>Parking and carpool info, public transit</p>
+            <InfoCard icon="🧺" title="What to bring">
+                <p>Mainly just bring yourself &lt;3 but here are some other things to consider:</p>
+                <ul>
+                    <li>A blanket or lawn chair</li>
+                    <li>Sunscreen</li>
+                    <li>A dish to share</li>
+                    <li>Board games or sports balls</li>
+                    <li>A water bottle to stay hydrated</li>
+                    <li>Layers for the afternoon breeze</li>
+                </ul>
             </InfoCard>
             <InfoCard icon="🤝" title='Accessibility'>
                 <p>If you are not feeling well or have been in contact with someone who is ill, please stay home!</p>
+                <p>Park in the official lots or taking the park shuttle to minimize walking distance</p>
+                <p>Consider masking to prevent the spread of disease and also protect against allergens</p>
+                <p>Restrooms are a short walk from the picnic area</p>
+                <p>There are paved paths around the parking lots and lake but uneven grass around picnic areas</p>
+            </InfoCard>
+            <InfoCard icon="🚌" title='Getting there'>
+                <b>Official lots</b>
+                <p>The park has several official lots that are a short walk from the picnic areas. They cost $10 for the day and you may have to endure a long line of cars at the park entrance. </p>
+                <b>Street parking</b>
+                <p>There is pedestrian entrance gate for the park on Cloverdale Ave (route to 4260 Cloverdale) and plentiful street parking in the residential area. The gate is a ~10 minute walk from the picnic areas.</p>
+                <b>Transit</b>
+                <p>There is <a href="https://pw.lacounty.gov/core-service-areas/transportation/the-link-baldwin-hills-parklands/">a shuttle</a> that runs from the Expo/La Cienega metro station to the park.</p>
             </InfoCard>
         </div>
-        <div className='max-w-2xl'>
-            <InfoCard icon="🍓" title="Food & drinks">
-                <p>kuku sabzi, kotlet, chai, sabzi khordan. Let me know if you have dietary restrictions or preferences. Contributions are welcome but not required. Hit me up if you want help deciding what to bring!</p>
-            </InfoCard>
-        </div>
-        
-        <Separator className="my-4 max-w-2xl"/>
-        <Card className="bg-foreground p-4 text-background w-full max-w-2xl">
-            <h2 className="font-sans text-2xl text-accent">Will you join us?</h2>
-            <p>Please respond by April 1</p>
-            <form>
-                <FieldGroup>
-                    <Field>
-                        <FieldLabel htmlFor='form-name'>Your name</FieldLabel>
-                        <Input
-                            id='form-name'
-                            type='text'
-                            placeholder="Luigi Mangionne"
-                            required
-                        />
-                    </Field>
-                    <Field>
-                        <FieldLabel htmlFor="form-attending">
-                            Attending?
-                        </FieldLabel>
-                        <Select defaultValue="">
-                            <SelectTrigger id="form-attending">
-                            <SelectValue placeholder="-- Select --" />
-                            </SelectTrigger>
-                            <SelectContent>
-                            <SelectGroup>
-                                <SelectItem value="Yes">Yes</SelectItem>
-                                <SelectItem value="No">No</SelectItem>
-                                <SelectItem value="Maybe">Maybe</SelectItem>
-                            </SelectGroup>
-                            </SelectContent>
-                        </Select>
-                    </Field>
-                    <Field>
-                        <FieldLabel htmlFor="form-attending">
-                            Number of guests
-                        </FieldLabel>
-                        <Select defaultValue="1">
-                            <SelectTrigger id="form-attending">
-                            <SelectValue placeholder="" />
-                            </SelectTrigger>
-                            <SelectContent>
-                            <SelectGroup>
-                                <SelectItem value="1">Just me (1)</SelectItem>
-                                <SelectItem value="2">Me and a plus one (2)</SelectItem>
-                                <SelectItem value="3">Me n my polycule (3+, please lmk how many!)</SelectItem>
-                            </SelectGroup>
-                            </SelectContent>
-                        </Select>
-                    </Field>
-                    <Field>
-                        <FieldLabel htmlFor="form-message">Dietary needs / dish you're bringing / message</FieldLabel>
-                        <Textarea id="form-message" placeholder="E.g. 'Bringing a fruit salad! Vegetarian-friendly.' or 'Will need a chair — thanks!'" />
-                    </Field>
-                    <Button type="submit" className="bg-accent text-ring">Submit</Button>
-                </FieldGroup>
-            </form>
-        </Card>
-        <Card className="w-full max-w-2xl bg-white my-4 p-4">
-            <h2 className="text-2xl font-sans text-foreground">Guest list</h2>
-            <div>
-                <div className="flex justify-between">
-                    <div>
-                       <h3 className="text-lg">Potato</h3>
-                        <span className="flex text-xs text-ring py-2 gap-2"><p>March 15</p> • <p>1 guest</p></span>
-                        <p className="text-border text-xs">meow meow meow</p> 
-                    </div>
-                    <Badge className="bg-muted-foreground text-ring">Attending</Badge>
-                </div>
-            </div>
-        </Card>
+        <Button className="bg-foreground p-4 text-background fixed bottom-4 right-6 p-6 rounded-full">
+            <a className="font-sans text-2xl text-accent" href="https://forms.gle/LVzDtEqBJS968uCz9" target="_blank" rel="noopener noreferrer">RSVP here</a>
+        </Button>
     </div>);
 }
 
